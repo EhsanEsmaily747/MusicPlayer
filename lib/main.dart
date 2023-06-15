6685376281130d14fb3307115b714776af5afc7e
EhsanEsmaily747 <ehsanesmaily747@gmail.com>
2023-06-15 13:06:58 +0430

    modal bottom sheet added and the picture the title and the singer part also completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,26 +19,111 @@ class MyApp extends StatelessWidget {
         primaryColor: Color(0xFF52524F),
         scaffoldBackgroundColor:  Color(0xFF52524F),
       ),
-      home:Scaffold(
+      home:Musicdesign()
+    );
+  }
+}
+
+class Musicdesign extends StatelessWidget {
+  const Musicdesign({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
       appBar: AppBar(
         
         titleTextStyle:TextStyle(
             fontFamily:'Inika',
             fontSize: 27.0,
+            fontWeight: FontWeight.bold
         ),
         centerTitle: true,
-        toolbarHeight: 80,
+        toolbarHeight: 70,
         elevation: 0,
         backgroundColor:Color(0xFF52524F) ,
         title: Text('Music Player'),
         actions: [
           IconButton(
             onPressed: (){
-
-          }, icon: Icon(Icons.menu_outlined))
+              showModalBottomSheet(
+                context: context,
+                 builder: (BuildContext context){
+                  return Container(
+                    height: 500.0,
+                    child: Column(
+                      children: [],
+                    ),
+                  );
+                 },
+                 showDragHandle: true,
+                 backgroundColor: Color(0xFF52524F)
+                 );
+          }, icon: Icon(Icons.menu_outlined)
+          ,)
         ],
-      ),                
-      )
-    );
+      ),       
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50.0,
+          ),
+          
+          Text('Listen To Your Favorite Song Here',style: TextStyle(
+            color: Color(0xFFD9D9D9),
+            fontSize: 16.0,
+            // fontStyle: FontStyle.italic,
+            fontFamily: 'Inika',
+            
+
+          ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Center(
+            child: Container(
+              width: 240.0,
+              height: 240.0,
+              decoration: BoxDecoration(
+                
+                borderRadius: BorderRadius.circular(140.0),
+                image: DecorationImage(image: AssetImage('images/Dusk-Till-Dawn.jpg')),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset.fromDirection(7.8,18.0),
+                    color:Color.fromARGB(255, 166, 166, 166),
+                    blurRadius: 50.0,
+                    spreadRadius: 2.0,
+                    blurStyle: BlurStyle.normal,
+                  )
+                ]
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Text('DUSK TILL DAWN',
+            style: TextStyle(
+              color:Color(0xFFD9D9D9),
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          Text('ZAYN & SIA',
+            style: TextStyle(
+              color: Color(0xFFD9D9D9),
+              fontSize: 16.0
+            ),
+            ),
+
+
+        ],
+      ),         
+      );
   }
 }
